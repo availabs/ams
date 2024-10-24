8d04dd70c22428595f3e4ddacbbd2b9b59803c45
Ryan Dubowsky <r.k.dubowsky@gmail.com>
2024-10-24 10:41:34 -0400

    users table more stuff

--- src/components/ams-users.jsx
+++ src/components/ams-users.jsx
@@ -9,7 +9,7 @@ export default usersWrapper((props) => {
   console.log(props.users);
   return (
     <div className="h-full bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8 ">
-      <Table data={props.users} columns={COLUMNS} />
+      <Table data={props.users} columns={COLUMNS} pageSize={10} striped/>
     </div>
   );
 });
@@ -17,26 +17,50 @@ export default usersWrapper((props) => {
 const DateCell = ({ value, ...props }) => {
   const myDate = new Date(value.replace(/"/g, ""));
 
-  return <div>{myDate.toLocaleString()}</div>;
+  return <div>{myDate.toLocaleDateString()}</div>;
 };
+
+const COLUMN_FILTER_PROPS = {
+  filterClassName: 'bg-blue-50 rounded',
+  filter: "text",
+  filterPlaceholder: "",
+}
+
 const COLUMNS = [
-  { accessor: "", Header: "Display Name" }, //TODO
+  { accessor: "", Header: "Display Name"}, //TODO
   {
     accessor: "email",
     Header: "Email",
     searchable: true,
-    filter: "text",
-    filterPlaceholder: "zzs",
-    filterThemeOptions: { size: "mini" },
-    filterClassName: "w-full text-sm z-50",
+    ...COLUMN_FILTER_PROPS
   },
   {
     accessor: (d) => d.groups.join(", "),
     Header: "Agency",
+    minWidth:"150px",
+    ...COLUMN_FILTER_PROPS
   },
-  { accessor: "", Header: "Roles" }, //TODO -- for now, map thru `projects`, get AuthLevel
+  { accessor: (d) => d.projects.map(p => AUTH_TO_ROLE[p.auth_level]).filter(onlyUnique).join(", "),
+    Header: "Roles", 
+    minWidth:"150px",
+    ...COLUMN_FILTER_PROPS
+  },
 
-  { accessor: "created_at", Header: "Registered", Cell: DateCell },
+  { accessor: "created_at", Header: "Registered", Cell: DateCell, maxWidth:'125px' },
   { accessor: "", Header: "Logins" }, //TODO
   { accessor: "", Header: "Last Login" }, //TODO
+  { accessor:"", Header: "Change Role"}, //TODO
+  { accessor:"", Header: "Delete User"} //TODO
 ];
+
+function onlyUnique(value, index, array) {
+  return array.indexOf(value) === index;
+}
+const AUTH_TO_ROLE = { 
+ 10: 'System Admin',
+ 5: 'Agency Admin',
+ 1: 'Agency User', 
+ 5: 'Contributor', 
+ 5: 'Librarian', 
+ 0: 'Public'
+}
