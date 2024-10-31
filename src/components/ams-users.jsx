5ee469b4f604066eabbcae69e944ebd2c667cb4e
Ryan Dubowsky <r.k.dubowsky@gmail.com>
2024-10-31 10:08:40 -0400

    can now sort date joined column
    can now filter and sort by display_name

--- src/components/ams-users.jsx
+++ src/components/ams-users.jsx
@@ -7,68 +7,77 @@ export default usersWrapper((props) => {
   return (
     <div className="h-full bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8 ">
       <div className="text-xl mb-4">Users</div>
-      <Table data={props.users} columns={COLUMNS} pageSize={10} striped/>
+      <Table
+        data={props.users}
+        columns={COLUMNS}
+        pageSize={10}
+        striped
+      />
     </div>
   );
 });
 
 const DateCell = ({ value, ...props }) => {
-  const myDate = new Date(value.replace(/"/g, ""));
-
-  return <div>{myDate.toLocaleDateString()}</div>;
+  return <div>{value.toLocaleDateString()}</div>;
 };
-
-
-const NameCell = (props) => {
-  return (
-    <div>
-      {props.row.original?.preferences?.display_name || ''}
-    </div>
-  );
-};
-
 
 const COLUMN_FILTER_PROPS = {
-  filterClassName: 'bg-blue-50 rounded',
+  filterClassName: "bg-blue-50 rounded",
   filter: "text",
   filterPlaceholder: "",
-}
+};
 
 const COLUMNS = [
-  { accessor: "", Header: "Display Name", Cell: NameCell,},
+  {
+    accessor: (d) => d?.preferences?.display_name ?? "",
+    Header: "Display Name",
+    ...COLUMN_FILTER_PROPS,
+  },
   {
     accessor: "email",
     Header: "Email",
-    searchable: true,
-    ...COLUMN_FILTER_PROPS
+    ...COLUMN_FILTER_PROPS,
   },
   {
     accessor: (d) => d.groups.join(", "),
     Header: "Agency",
-    minWidth:"150px",
-    ...COLUMN_FILTER_PROPS
+    minWidth: "150px",
+    ...COLUMN_FILTER_PROPS,
   },
-  { accessor: (d) => d.projects.map(p => AUTH_TO_ROLE[p.auth_level]).filter(onlyUnique).join(", "),
-    Header: "Roles", 
-    minWidth:"150px",
-    ...COLUMN_FILTER_PROPS
+  {
+    accessor: (d) =>
+      d.projects
+        .map((p) => AUTH_TO_ROLE[p.auth_level])
+        .filter(onlyUnique)
+        .join(", "),
+    Header: "Roles",
+    minWidth: "150px",
+    ...COLUMN_FILTER_PROPS,
   },
 
-  { accessor: "created_at", Header: "Registered", Cell: DateCell, maxWidth:'125px' },
+  {
+    accessor: (d) => new Date(d.created_at.replace(/"/g, "")),
+    Cell: DateCell,
+    sortType: "datetime",
+    Header: "Registered",
+    maxWidth: "125px",
+    ...COLUMN_FILTER_PROPS,
+    filter:""
+  },
   { accessor: "", Header: "Logins" }, //TODO
   { accessor: "", Header: "Last Login" }, //TODO
-  { accessor:"", Header: "Change Role"}, //TODO
-  { accessor:"", Header: "Delete User"} //TODO
+  { accessor: "", Header: "Change Role" }, //TODO
+  { accessor: "", Header: "Delete User" }, //TODO
 ];
 
 function onlyUnique(value, index, array) {
   return array.indexOf(value) === index;
 }
-const AUTH_TO_ROLE = { 
- 10: 'System Admin',
- 5: 'Agency Admin',
- 1: 'Agency User', 
- 5: 'Contributor', 
- 5: 'Librarian', 
- 0: 'Public'
-}
+const AUTH_TO_ROLE = {
+  10: "System Admin",
+  5: "Agency Admin",
+  1: "Agency User",
+  5: "Contributor",
+  5: "Librarian",
+  0: "Public",
+};
